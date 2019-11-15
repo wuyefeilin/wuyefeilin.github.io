1720b5fb2cb4a0bc199ac883fd9219599644cef1
Cyating <924471303@qq.com>
2019-11-15 11:26:56 +0800

    修改配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,7 @@
     description: '用心写代码，不辜负程序员之名',//网站描述
     //head标签
     head: [
-        ['link', { rel: 'stylesheet', href: '/img/logo.ico' }],//注意"/"就是public资源目录。标签的logo
+        ['link', { rel:'icon',type:'image/x-icon',  href: '/img/logo.ico' }],//注意"/"就是public资源目录。标签的logo
     ],
     themeConfig: {
         author:'tinger',
