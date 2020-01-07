b9fd816b8cffd759b3c1a9de898a9f024f464e30
sushe <924471303@qq.com>
2020-01-07 21:41:05 +0800

    add live2d

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,6 +4,7 @@
     //head标签
     head: [
         ['link', { rel:'icon',type:'image/x-icon',  href: '/img/logo.ico' }],//注意"/"就是public资源目录。标签的logo
+        ['script', { src: 'https://cdn.bootcss.com/jquery/3.4.1/jquery.js' }]
     ],
     themeConfig: {
         author:'tinger',
